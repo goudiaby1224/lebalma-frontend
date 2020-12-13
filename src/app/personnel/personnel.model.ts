14ab6191f0ce5e65a97eb836dfe5f2d7861d9f37
Noel Goudiaby <goudiaby1224@gmail.com>
2020-12-13 12:46:53 -0500

    Remove unnecessary fields

--- src/app/personnel/personnel.model.ts
+++ src/app/personnel/personnel.model.ts
@@ -8,13 +8,9 @@ export class Personnel {
   public nomUtilisateur: string;
   public mail: string;
   public tel: string;
-  public login: string;
-  public password: string;
-  public numero: number;
   public role: string;
 
-  constructor(id: number, nom: string, prenom: string, adresse: string, metier: string, service: string, nomUtilisateur: string, mail: string, login: string, password: string,
-              numero: number, role: string) {
+  constructor(id: number, nom: string, prenom: string, adresse: string, metier: string, service: string, nomUtilisateur: string, mail: string, role: string) {
     this.id = id;
     this.nom = nom;
     this.prenom = prenom;
@@ -24,9 +20,6 @@ export class Personnel {
     this.nomUtilisateur = service;
     this.mail = mail;
     this.tel = service;
-    this.login = login;
-    this.password = password;
-    this.numero = numero;
     this.role = nom;
   }
 }
