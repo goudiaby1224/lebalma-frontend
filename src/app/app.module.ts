ec3b73c4980a3703812ef48132ce72e6093e29d0
=Lamine Doucoure <laminedouk2001@gmail.com>
2020-12-19 10:25:16 -0500

    user

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,27 +3,34 @@ import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
-import { UserComponent } from './user/user.component';
 import { PartenaireComponent } from './partenaire/partenaire.component';
 import { AdresseComponent } from './adresse/adresse.component';
 import { PersonnelComponent } from './personnel/personnel.component';
 import { EntrepriseComponent } from './entreprise/entreprise.component';
 import {FormsModule} from '@angular/forms';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { DefaultModule } from './layouts/default/default.module';
+import { MaterialModule } from './material/material.module';
+
 
 @NgModule({
   declarations: [
     AppComponent,
-    UserComponent,
     PartenaireComponent,
     AdresseComponent,
     PersonnelComponent,
     EntrepriseComponent
+ 
   ],
-    imports: [
-        BrowserModule,
-        AppRoutingModule,
-        FormsModule
-    ],
+  imports: [
+    BrowserModule,
+    AppRoutingModule,
+    BrowserAnimationsModule,
+   DefaultModule,
+   MaterialModule,
+   FormsModule
+  ],
+    
   providers: [],
   bootstrap: [AppComponent]
 })
