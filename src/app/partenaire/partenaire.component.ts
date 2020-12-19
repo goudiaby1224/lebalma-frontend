c6d18734c446b7a1dda89625ecff98251d380b0e
Noel Goudiaby <goudiaby1224@gmail.com>
2020-12-18 23:28:12 -0500

    implementing http methods

--- src/app/partenaire/partenaire.component.ts
+++ src/app/partenaire/partenaire.component.ts
@@ -1,4 +1,7 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, EventEmitter, ViewChild, ElementRef } from '@angular/core';
+import {HttpClient, HttpClientModule} from '@angular/common/http';
+import { Partenaire } from './partenaire.model';
+import {map} from 'rxjs/operators';
 
 @Component({
   selector: 'app-partenaire',
@@ -6,10 +9,38 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./partenaire.component.css']
 })
 export class PartenaireComponent implements OnInit {
-
-  constructor() { }
+  //@ViewChild('raisonSocialInput', { static: false }) raisonSocialInputRef: ElementRef;
+  //@ViewChild('adresseInput', { static: false }) adresseInputRef: ElementRef;
+  partenaireCreated = new EventEmitter<Partenaire>();
+  constructor(private http: HttpClient) { }
 
   ngOnInit(): void {
+  }
+
+  onCreate(partenaire: any): void {
+      this.partenaireCreated.emit(partenaire);
+      this.http.post('http://localhost:8881/users', partenaire).subscribe(responseData => {
+        console.log(responseData);
+      });
+  }
+
+  getPartenaire(idPartenaire: string): void {
+    this.http.get<{ [key: string]: Partenaire }>('http://localhost:8881/partenaires/id')
+      .pipe(
+        map(responseDataResponse  => {
+         console.log(responseDataResponse);
+         const postArray: Partenaire[] = [];
+         for (const key in responseDataResponse) {
+           if (responseDataResponse.hasOwnProperty(key)){
+             //postArray.push(responseDataResponse[key], id: key );
+           }
+         }
+         return postArray;
+      })
+      )
+      .subscribe(responseData => {
+       console.log(responseData);
+      });
   }
 
 }
